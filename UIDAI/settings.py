c7794cceeecfccc725b081ccde795ddc1ecfcc1b
Dharundds <76607616+Dharundds@users.noreply.github.com>
2021-10-29 14:56:05 +0530

    updates

--- UIDAI/settings.py
+++ UIDAI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Auth_api',
     'rest_framework',
 
 ]
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'UIDAI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
